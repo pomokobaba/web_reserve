f4b65ee5c7ae2898d898daa7880925f2af8318a9
maibaba <mbaba@coresoft-net.co.jp>
2022-10-06 21:16:50 +0900

    編集の追加

--- src/app/Providers/RouteServiceProvider.php
+++ src/app/Providers/RouteServiceProvider.php
@@ -47,6 +47,8 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
+
+        Route::pattern('tweetId', '[0-9]+');
     }
 
     /**
--- src/resources/views/tweet/index.blade.php
+++ src/resources/views/tweet/index.blade.php
@@ -1,36 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta 
-            name="viewport" 
-            content="width=device-width, user-scalable=no, initial-scale=1"
-        >
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>つぶやきアプリ</title>
-    </head>
-    <body>
-        <h1>つぶやきアプリ</h1>
-        <div>
-            <p>投稿フォーム</p>
-            {{-- form内にあるデータの送信先の指定と、送信するHTTPのメソッドを定義。
-                Routで名前を付けたので、routeヘルパーにその名前を指定することで対応したURLのバスを設定できる。 --}}
-            <form action="{{ route('tweet.create') }}" method="post">
-                @csrf
-                <label for="tweet-content">つぶやき</label>
-                <span>140文字まで</span>
-                <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
-                {{-- バリデーションに失敗したらエラーを返す --}}
-                @error('tweet')
-                <p style="color: red;">{{ $message }}</p>
-                @enderror
-                <button type="submit">投稿</button>
-            </form>
-        </div>
-        <div>
-            @foreach($tweets as $tweet)
-                <p>{{ $tweet->content }}</p>
-            @endforeach
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>つぶやきアプリ</title>
+</head>
+<body>
+    <h1>つぶやきアプリ</h1>
+    <div>
+        <p>投稿フォーム</p>
+        <form action="{{ route('tweet.create') }}" method="post">
+            @csrf
+            <label for="tweet-content">つぶやき</label>
+            <span>140文字まで</span>
+            <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+            @error('tweet')
+            <p style="color: red;">{{ $message }}</p>
+            @enderror
+            <button type="submit">投稿</button>
+        </form>
+    </div>
+    <div>
+    @foreach($tweets as $tweet)
+        <details>
+            <summary>{{ $tweet->content }}</summary>
+            <div>
+                <a href="{{ route('tweet.update.index', $tweet->id) }}">編集</a>
             </div>
-    </body>
+        </details>
+    @endforeach
+    </div>
+</body>
 </html>
