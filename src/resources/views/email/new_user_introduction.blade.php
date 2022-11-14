5317c4586833ba4f9a2a65533b683f78ca647c2d
maibaba <mbaba@coresoft-net.co.jp>
2022-11-14 21:26:48 +0900

    メール本文をHTML化する

--- src/resources/views/email/new_user_introduction.blade.php
+++ src/resources/views/email/new_user_introduction.blade.php
@@ -1,1 +1,15 @@
-{{ $toUser->name }} さんこんにちは！新しく{{ $newUser->name }}さんが参加されました。
+@component('mail::message')
+
+# 新しいユーザが追加されました！
+
+{{ $toUser->name }} さんこんにちは！
+
+@component('mail::panel')
+新しく{{ $newUser->name }}さんが参加されました😁
+@endcomponent
+
+@component('mail::button', ['url' => route('tweet.index')])
+    つぶやきを見に行く
+@endcomponent
+
+@endcomponent
